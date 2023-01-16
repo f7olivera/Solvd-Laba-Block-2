12978f15bc64b27fc1b70d7a33a9d48b930d116b
f7olivera <f7olivera@gmail.com>
2023-01-16 02:22:18 -0300

    Update PersonDAO.java

--- homework/src/main/java/com/solvd/buildingcompany/mysql/PersonDAO.java
+++ homework/src/main/java/com/solvd/buildingcompany/mysql/PersonDAO.java
@@ -1,1 +1,14 @@
+package com.solvd.buildingcompany.mysql;
+
+import com.solvd.buildingcompany.dao.IPersonDAO;
+import com.solvd.buildingcompany.people.Person;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
+
 public class PersonDAO implements IPersonDAO {
+}
