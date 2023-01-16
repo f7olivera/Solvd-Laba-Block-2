99d5370ace2c4db80ad42a33a9530ca9336ce3bd
f7olivera <f7olivera@gmail.com>
2023-01-16 17:48:53 -0300

    Use mysql.db.properties file

--- homework/src/main/java/com/solvd/buildingcompany/Main.java
+++ homework/src/main/java/com/solvd/buildingcompany/Main.java
@@ -7,8 +7,13 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Main {
     private final static Logger LOGGER = LogManager.getLogger(Main.class);
@@ -16,10 +21,16 @@ public class Main {
     public static void main(String[] args) throws SQLException {
         LOGGER.info("Starting program.");
         try (BasicDataSource dataSource = new BasicDataSource()) {
-            dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
-            dataSource.setUrl("jdbc:mysql://localhost:3306/Building_Company");
-            dataSource.setUsername("root");
-            dataSource.setPassword("root");
+            Properties properties = new Properties();
+            try (InputStream inputStream = Files.newInputStream(Paths.get("src/main/resources/mysql.db.properties"))) {
+                properties.load(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            dataSource.setDriverClassName(properties.getProperty("driver"));
+            dataSource.setUrl(properties.getProperty("base_url") + "Building_Company");
+            dataSource.setUsername(properties.getProperty("username"));
+            dataSource.setPassword(properties.getProperty("password"));
             dataSource.setInitialSize(5);
 
             Connection connection = dataSource.getConnection();
