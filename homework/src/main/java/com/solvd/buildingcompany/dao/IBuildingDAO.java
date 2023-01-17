f3b8fb9d1f1aad07055666b97e829ca0f31bcc27
f7olivera <f7olivera@gmail.com>
2023-01-17 10:15:03 -0300

    Add methods

--- homework/src/main/java/com/solvd/buildingcompany/dao/IBuildingDAO.java
+++ homework/src/main/java/com/solvd/buildingcompany/dao/IBuildingDAO.java
@@ -1,6 +1,21 @@
 package com.solvd.buildingcompany.dao;
 
 import com.solvd.buildingcompany.models.buildings.Building;
+import com.solvd.buildingcompany.models.buildings.BuildingCategory;
+import com.solvd.buildingcompany.models.buildings.ConstructionDetails;
+
+import java.util.List;
 
 public interface IBuildingDAO extends IBaseDAO<Building> {
+    Building getByName(String name);
+
+    List<Building> getByCategory(BuildingCategory category);
+
+    List<Building> getByDetails(ConstructionDetails details);
+
+    int getBuildingCostId(int costPerSquareMeter, int permitCost);
+
+    int getBuildingTimelineId(int constructionTime, int permitProcessTime);
+
+    int getBuildingTypeId(int capacity, int floors, BuildingCategory category, int squareMeters);
 }
