599c548e542fe3cd0c04f9b42f7255c4f2b93d70
f7olivera <f7olivera@gmail.com>
2023-01-17 23:25:05 -0300

    Move ConnectionPool logic to its own class

--- homework/src/main/java/com/solvd/buildingcompany/Main.java
+++ homework/src/main/java/com/solvd/buildingcompany/Main.java
@@ -1,45 +1,24 @@
 package com.solvd.buildingcompany;
 
+import com.solvd.buildingcompany.dao.mysql.ConnectionPool;
 import com.solvd.buildingcompany.dao.mysql.PersonDAO;
 import com.solvd.buildingcompany.models.people.Person;
-import org.apache.commons.dbcp2.BasicDataSource;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.sql.SQLException;
-import java.util.Properties;
 
 public class Main {
     private final static Logger LOGGER = LogManager.getLogger(Main.class);
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) throws Exception {
         LOGGER.info("Starting program.");
-        try (BasicDataSource dataSource = new BasicDataSource()) {
-            LOGGER.info("Reading properties file.");
-            Properties properties = new Properties();
-            try (InputStream inputStream = Files.newInputStream(Paths.get("src/main/resources/mysql.db.properties"))) {
-                properties.load(inputStream);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-
-            LOGGER.info("Creating connection pool to MySQL database");
-            dataSource.setDriverClassName(properties.getProperty("driver"));
-            dataSource.setUrl(properties.getProperty("base_url") + "Building_Company");
-            dataSource.setUsername(properties.getProperty("username"));
-            dataSource.setPassword(properties.getProperty("password"));
-            dataSource.setInitialSize(5);
-
+        try (PersonDAO personDAO = new PersonDAO()) {
             Person person = new Person("John Doe", 34, 1245);
             LOGGER.info("Creating person with id " + person.getId() + ".");
-            PersonDAO personDAO = new PersonDAO(dataSource.getConnection());
             personDAO.create(person);
 
             LOGGER.info("Person found:\n" + personDAO.get(person.getId()));
+
+            ConnectionPool.getInstance().close();
         }
     }
 }
