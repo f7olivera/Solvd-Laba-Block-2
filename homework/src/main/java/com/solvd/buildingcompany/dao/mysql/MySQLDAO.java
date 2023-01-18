58fa2253680aa0086891ddb0e0b3ce75491e9ccd
f7olivera <f7olivera@gmail.com>
2023-01-17 23:23:09 -0300

    Implement AutoCloseable

--- homework/src/main/java/com/solvd/buildingcompany/dao/mysql/MySQLDAO.java
+++ homework/src/main/java/com/solvd/buildingcompany/dao/mysql/MySQLDAO.java
@@ -1,4 +1,21 @@
 package com.solvd.buildingcompany.dao.mysql;
 
-public abstract class MySQLDAO {
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public abstract class MySQLDAO implements AutoCloseable {
+    Connection connection;
+    private final static Logger LOGGER = LogManager.getLogger(MySQLDAO.class);
+
+    @Override
+    public void close() {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            LOGGER.info("Could not close connection.");
+        }
+    }
 }
