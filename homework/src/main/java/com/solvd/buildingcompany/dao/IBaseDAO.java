facdda5a3fcddc03235c090410258d72fe7e2c7c
f7olivera <f7olivera@gmail.com>
2023-01-19 01:58:27 -0300

    Add exception to signatures

--- homework/src/main/java/com/solvd/buildingcompany/dao/IBaseDAO.java
+++ homework/src/main/java/com/solvd/buildingcompany/dao/IBaseDAO.java
@@ -1,17 +1,18 @@
 package com.solvd.buildingcompany.dao;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface IBaseDAO<T> extends AutoCloseable {
-    T create(T t);
+    T create(T t) throws SQLException;
 
-    T get(int id);
+    T get(int id) throws SQLException;
 
-    T update(T t);
+    T update(T t) throws SQLException;
 
-    void delete(int id);
+    void delete(int id) throws SQLException;
 
-    List<T> findAll();
+    List<T> findAll() throws SQLException;
 
-    int count();
+    int count() throws SQLException;
 }
