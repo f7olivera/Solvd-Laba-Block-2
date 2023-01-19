620efb4eca1844252edfff3e46b0af312468ce88
f7olivera <f7olivera@gmail.com>
2023-01-19 02:04:51 -0300

    Refactor create method

--- homework/src/main/java/com/solvd/buildingcompany/dao/mysql/BuildingDAO.java
+++ homework/src/main/java/com/solvd/buildingcompany/dao/mysql/BuildingDAO.java
@@ -2,50 +2,64 @@ package com.solvd.buildingcompany.dao.mysql;
 
 import com.solvd.buildingcompany.dao.IBuildingDAO;
 import com.solvd.buildingcompany.models.building.Building;
-import com.solvd.buildingcompany.models.building.BuildingCategory;
-import com.solvd.buildingcompany.models.building.ConstructionDetails;
+import com.solvd.buildingcompany.models.building.details.BuildingCategory;
+import com.solvd.buildingcompany.models.building.details.BuildingCost;
+import com.solvd.buildingcompany.models.building.details.BuildingTimeline;
+import com.solvd.buildingcompany.models.building.details.BuildingType;
 import com.solvd.buildingcompany.models.construction.Item;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class BuildingDAO extends MySQLDAO implements IBuildingDAO {
-    private final Connection connection;
     private final static Logger LOGGER = LogManager.getLogger(BuildingDAO.class);
 
-    public BuildingDAO(Connection connection) {
-        this.connection = connection;
+    public BuildingDAO() throws SQLException {
+        super();
     }
 
     @Override
-    public Building create(Building building) {
-        ConstructionDetails details = building.getDetails();
-        LOGGER.info("Creating " + details.getCategory() + " building.");
+    public Building create(Building building) throws SQLException {
+        BuildingCost cost = building.getCost();
+        BuildingTimeline timeline = building.getTimeline();
+        BuildingType type = building.getType();
+
+        LOGGER.info("Creating " + type.getCategory() + " building.");
 
         // Searching costId, timelineId and typeId will create them if they don't exist
-        int costId = getBuildingCostId(details.getCostPerSquareMeter(), details.getPermitCost());
-        int timelineId = getBuildingTimelineId(details.getConstructionTime(), details.getPermitProcessTime());
-        int typeId = getBuildingTypeId(details.getCapacity(), details.getFloors(), details.getCategory(), details.getSquareMeters());
+        int costId = getBuildingCostId(cost.getCostPerSquareMeter(), cost.getPermitCost());
+        int timelineId = getBuildingTimelineId(timeline.getConstructionTime(), timeline.getPermitProcessTime());
+        int typeId = getBuildingTypeId(type.getCapacity(), type.getFloors(), type.getCategory(), type.getSquareMeters());
 
-        try {
-            String sql = "INSERT INTO Buildings (name, building_cost_id, building_timeline_id, building_type_id)" +
-                    "VALUES (?, ?, ?, ?)";
-            PreparedStatement statement = connection.prepareStatement(sql);
+        String sql = "INSERT INTO Buildings (name, building_cost_id, building_timeline_id, building_type_id)" +
+                "VALUES (?, ?, ?, ?)";
+        PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-            statement.setString(1, building.getName());
-            statement.setInt(2, costId);
-            statement.setInt(3, timelineId);
-            statement.setInt(4, typeId);
-            statement.executeUpdate();
-        } catch (SQLException e) {
-            LOGGER.error(e.getMessage());
+        statement.setString(1, building.getName());
+        statement.setInt(2, costId);
+        statement.setInt(3, timelineId);
+        statement.setInt(4, typeId);
+        statement.executeUpdate();
+
+        ResultSet keys = statement.getGeneratedKeys();
+        int generatedId = 0;
+        if (keys.next()) {
+            generatedId = keys.getInt(1);
+        }
+
+        String itemQuery = "INSERT INTO BuildingItems (building_id, item_id) VALUES (?, SELECT id FROM Items WHERE name = ? AND price = ?)";
+        PreparedStatement itemStatement = connection.prepareStatement(itemQuery);
+        itemStatement.setInt(1, generatedId);
+        for (Item item : building.getRequiredItems()) {
+            itemStatement.setString(2, item.getName());
+            itemStatement.setDouble(3, item.getPrice());
+            itemStatement.executeUpdate();
         }
         return building;
     }
@@ -94,10 +108,6 @@ public class BuildingDAO extends MySQLDAO implements IBuildingDAO {
         return null;
     }
 
-    @Override
-    public List<Building> getByDetails(ConstructionDetails details) {
-        return null;
-    }
 
     @Override
     public int getBuildingCostId(int costPerSquareMeter, int permitCost) {
