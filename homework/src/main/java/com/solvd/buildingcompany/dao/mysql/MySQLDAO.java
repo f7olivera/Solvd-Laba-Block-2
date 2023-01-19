1352a2ddb3c6be00ee4d5391a1455fc8b1813042
f7olivera <f7olivera@gmail.com>
2023-01-19 02:01:18 -0300

    Implement createIfNotExists

--- homework/src/main/java/com/solvd/buildingcompany/dao/mysql/MySQLDAO.java
+++ homework/src/main/java/com/solvd/buildingcompany/dao/mysql/MySQLDAO.java
@@ -4,11 +4,30 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public abstract class MySQLDAO implements AutoCloseable {
-    Connection connection;
-    private final static Logger LOGGER = LogManager.getLogger(MySQLDAO.class);
+    protected Connection connection;
+    protected final static Logger LOGGER = LogManager.getLogger(MySQLDAO.class);
+
+    public MySQLDAO() throws SQLException {
+        this.connection = ConnectionPool.getInstance().getConnection();
+    }
+
+    public int createIfNotExists(PreparedStatement insertStatement, PreparedStatement selectStatement) {
+        try {
+            insertStatement.executeUpdate();
+            ResultSet idResultSet = selectStatement.executeQuery();
+
+            if (idResultSet.next())
+                return idResultSet.getInt("id");
+        } catch (SQLException e) {
+            LOGGER.error(e.getMessage());
+        }
+        return 0;
+    }
 
     @Override
     public void close() {
